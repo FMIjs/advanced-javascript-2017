f7b866f99bf787168d8309fe1d74b745e20ba65c
Aleksander Kostov <akostov94@gmail.com>
2017-11-28 18:56:07 +0200

    Project Structure

--- exercises/exercise6/dist/db/user.js
+++ exercises/exercise6/dist/db/user.js
@@ -22,20 +22,8 @@ var UserStructure = (function () {
 var Users = (function (_super) {
     __extends(Users, _super);
     function Users() {
-        var _this = _super.call(this, 'users', UserStructure) || this;
-        _this.findByEmail = function (email) { return _this._db.entities[_this._db.emails.indexOf(email)]; };
-        _this.findByUsername = function (username) { return username ?
-            _this._db.entities.find(function (user) { return (user.firstName + "." + user.lastName).toLowerCase() === username.toLowerCase(); }) : null; };
-        _this.findById = function (id) { return id ?
-            _this._db.entities.find(function (user) { return user._id === id; }) : null; };
-        return _this;
+        return _super.call(this) || this;
     }
-    Users.prototype.insert = function (user) {
-        if (this._db.emails.indexOf(user.email) !== -1)
-            return Promise.reject(new Error('User already registered!'));
-        this._db.emails.push(user.email);
-        return _super.prototype.insert.call(this, user);
-    };
     return Users;
 }(base_1.DbModel));
 exports.Users = Users;
--- exercises/exercise6/src/db/user.ts
+++ exercises/exercise6/src/db/user.ts
@@ -15,22 +15,10 @@ class UserStructure implements IDbStructure<User> {
 
 export class Users extends DbModel<User, UserStructure> {
   constructor() {
-    super('users', UserStructure);
+    super(/* ... */);
+    // ...
   }
-
-  insert(user: User) {
-    if (this._db.emails.indexOf(user.email) !== -1) return Promise.reject(new Error('User already registered!'));
-    this._db.emails.push(user.email);
-    return super.insert(user);
-  }
-
-  findByEmail = (email: string): User => this._db.entities[this._db.emails.indexOf(email)];
-
-  findByUsername = (username: string): User => username ?
-    this._db.entities.find(user => `${user.firstName}.${user.lastName}`.toLowerCase() === username.toLowerCase()) : null;
-
-  findById = (id: number): User => id ?
-    this._db.entities.find(user => user._id === id) : null;
+  // ...
 }
 
 export default new Users();
