5e8faae6f454530caa2c215db13609ce7ad68110
Laro DEE <larodi@gmail.com>
2017-11-28 22:39:13 +0200

    clearup & instructions

--- exercises/exercise6/mixins-again.js
+++ exercises/exercise6/mixins-again.js
@@ -1,4 +1,4 @@
-
+// mixins in ES6 - the str8fwd way
 class Testov {
   constructor () {
     console.log('construct an object')
@@ -6,20 +6,36 @@ class Testov {
   }
 }
 
-let Doing = function() {
+let Doing = function () {
 
 }
 
 Doing.prototype = {
-  method: function() {
-    console.log(`i know ho to use ${this.somevar}`)
+  method: function () {
+    console.log(`i knoo hau 2 use [${this.somevar}]`)
   }
 }
 
+// no instancing from the mixin directly
+let d = new Doing()
+d.method()
+
 let t = new Testov()
 
+try {
+  t.method()
+} catch (e) {
+  console.warn('okay, were now totally convinced')
+}
+
+//  quick way to mixin the entire class
+//  back in ES5 we used to do it with forloop
 Object.assign(Testov.prototype, Doing.prototype)
+
+// now t has this method()
 t.method()
 
+// now this new t2 guy will have  it also, as we
+// mixed in the Doing's proto, thus all its members 
 let t2 = new Testov()
 t2.method()
