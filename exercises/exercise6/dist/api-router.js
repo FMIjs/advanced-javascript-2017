5ba0fcb90e2ff9db556b598ed2328616ffd1078c
Aleksander Kostov <akostov94@gmail.com>
2017-12-05 17:17:28 +0200

    .

--- exercises/exercise6/dist/api-router.js
+++ exercises/exercise6/dist/api-router.js
@@ -2,6 +2,16 @@
 exports.__esModule = true;
 var express_1 = require("express");
 exports.apiRouter = express_1.Router();
+var SortStrings = (function () {
+    function SortStrings() {
+    }
+    SortStrings.prototype.sort = function () {
+    };
+    return SortStrings;
+}());
+var sort = new SortStrings;
+sort.elements = ['1', '2', '3'];
+sort.elements.push('1');
 exports.apiRouter.get('/', function (req, res) {
     res.send("Hello from the API").end();
 });
--- exercises/exercise6/src/api-router.ts
+++ exercises/exercise6/src/api-router.ts
@@ -1,9 +1,30 @@
 import { Router } from 'express';
-import Users from './db/user';
+// import Users from './db/user';
 
-import { User } from './models/user';
+// import { User } from './models/user';
 
 export const apiRouter = Router();
+
+interface ISortable<T, V> {
+  elements: T[],
+  comparator(first: T, second: T): V,
+}
+
+interface ISortAlg<T, V> {
+  comparator: ISortable<T, V>
+  sort(): void
+}
+
+
+
+
+interface BubleSort<T> extends ISortAlg<T, boolean> {
+  
+}
+
+// const sort = new SortStrings;
+// sort.elements = ['1','2','3'];
+// sort.elements.push('1');
 
 apiRouter.get('/', (req, res) => {
   res.send(`Hello from the API`).end();
