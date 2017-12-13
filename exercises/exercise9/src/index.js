974d81e03d33079936992de7accaedab00898247
Laro DEE <larodi@gmail.com>
2017-12-13 10:58:31 +0200

    annotate

--- exercises/exercise9/src/index.js
+++ exercises/exercise9/src/index.js
@@ -8,16 +8,23 @@ app.use(express.static('public'))
 
 let cnum = 0;
 
+// serve app.js statically
+// normally app.js should be in the 
+// public dir - minified and webpacked
 app.get('/app.js',  (req, res) => {
   res.sendfile(__dirname + '/app.js');
 });
 
+// this effectively emits via the channel
 function emitData(socket) {
   let data = Math.random()*100;
   socket.emit('DATA', data);
   console.log(`emitted [${data}] to client [${cnum}]`);
 }
 
+// on connection...
+// it is worth nothign that there's also
+// somethign called socket.io-stream
 io.on('connection', function (socket) {
   cnum++;
   socket.emit('SERVICE', { status: 'connected' });
